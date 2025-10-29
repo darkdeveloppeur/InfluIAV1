5a27304f2b9883ddcf7be2c7b0f5af1a6030fcaa
darkdeveloppeur <darkdeveloppeur@gmail.com>
2025-10-29 19:31:21 +0100

    Create config.js

---
+++ js/auth/config.js
@@ -0,0 +1,3 @@
+// Remplace par tes VRAIES clés Supabase (celles du NOUVEAU projet)
+export const SUPABASE_URL = 'TON_NOUVEL_URL_SUPABASE';
+export const SUPABASE_KEY = 'TA_NOUVELLE_CLE_ANON_PUBLIC';
